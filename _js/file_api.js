61c01f3c729573d219e5d18ade88704ff273e149
Marcelo Oliveira <marcelo.oliveira@iceland2.com>
2012-12-03 16:01:38 -0200

    image file is being loaded and draw on the canvas. Click events already paint canvas.

--- _js/file_api.js
+++ _js/file_api.js
@@ -1,39 +1,15 @@
-var canvas = document.getElementById('canvas'),
-    ctx = canvas.getContext('2d');
+main.fileAPI = function (el, callback) {
+    document.getElementById(el).addEventListener('change', function (e) {
+
+        //console.log(e.target.files)
+        var reader = new FileReader();
+
+        // Closure to capture the file information.
+        reader.onload = callback;
 
 
+        // Read in the image file as a data URL.
+        reader.readAsDataURL(e.target.files[0]);
 
-document.getElementById('fileInput').addEventListener('change', function (e) {
-
-  //console.log(e.target.files)
-
-  var reader = new FileReader();
-
-  // Closure to capture the file information.
-  reader.onload = doMagic;
-
-
-  // Read in the image file as a data URL.
-  reader.readAsDataURL(e.target.files[0]);
-
-});
-
-function doMagic (e) {
-  // Render
-  var img = new Image();
-
-  img.onload = function() {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img, 0, 0);
-
-    var dataURL = canvas.toDataURL();
-
-      // set canvasImg image src to dataURL
-      // so it can be saved as an image
-      document.getElementById('image').src = dataURL;
-  }
-
-  img.src = e.target.result;
-
+    });
 };
