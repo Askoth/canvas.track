a46f90dfff7f5cc19f0fbb7f91ac9de53bc36776
Marcelo Oliveira <askoth@gmail.com>
2013-02-24 16:46:02 -0300

    now you can set a brackground color to help when images have transparency or are white

--- _js/base.js
+++ _js/base.js
@@ -125,4 +125,28 @@
 
     });
 
+    document.getElementById('inp-bg-color').addEventListener('change', function () {
+        document.getElementById('canvas-wrap').style.backgroundColor = this.value;
+    });
+
+    document.getElementById('inp-bg-alpha').addEventListener('change', function () {
+
+        var color = document.getElementById('canvas-wrap').style.backgroundColor,
+            result = '';
+
+        color = color.replace(/[^0-9|,]/g, '');
+
+        color = color.split(',').slice(0, 3);
+
+        if (color.length < 3) {
+            //not rgb(a) format
+            return;
+        }
+
+        result = 'rgba(' + color.join(',') + ',' + this.value/100 + ')';
+        console.log(document.getElementById('canvas-wrap').style.backgroundColor);
+
+        document.getElementById('canvas-wrap').style.backgroundColor = result;
+    });
+
 })(window.main);
